276fd04f5cf4a5c154cfe9765ce88edb79e5c59a
perederiymaxim <dendibaby@gmail.com>
2020-06-11 21:56:12 +0300

    Delete extra file

--- DevartProductsProject/driverSummon.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-
-class driverSummon():
-    def __init__(self, driver):
-        self.driver = driver
-
-
